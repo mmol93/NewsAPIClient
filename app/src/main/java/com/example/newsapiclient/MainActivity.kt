a2191194efbe8cb34cc51d172343cefd0cb9f2dc
ljws93 <ljws93@naver.com>
2021-11-29 23:30:13 +0900

    make basic fragments and connect with bottomTab navigation

--- app/src/main/java/com/example/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/example/newsapiclient/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // fragment를 bottomTab과 연결한다
+        val navHostFragment =
+            supportFragmentManager
+                .findFragmentById(R.id.newsFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(navController)
     }
 }
